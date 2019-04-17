e41fee9f6bb7833fccc84b30542153a41c71131a
jthicks91 <jordanthicks91@gmail.com>
2019-04-16 19:10:10 -0700

    set up index with dependencies, built actions and reducer. moving on to wiring component tree.

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,17 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from "axios";
+
+export const FETCH_CHARACTER_START = "FETCH_CHARACTER_START";
+export const FETCH_CHARACTER_SUCCESS = "FETCH_CHARACTER_SUCCESS";
+export const FETCH_CHARACTER_FAILURE = "FETCH_CHARACTER_FAILURE";
+
+export const getCharacter = () => dispatch => {
+  dispatch({ type: FETCH_CHARACTER_START });
+  axios
+    .get(`https://swapi.co/api/people/`)
+    .then(res => dispatch({ type: FETCH_CHARACTER_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: FETCH_CHARACTER_FAILURE, payload: err }));
+};
